e51496fd9154be73182c3de287608d4b7a71d9ee
Олег Хазаров <Nem4omania@outlook.com>
2019-11-19 21:32:35 +0200

    Добавьте файлы проекта.

---
+++ Zadanie__3_2_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/*Найти количество элементов массива, встречающихся в нем
+один раз.*/
+
+
+class Zadanie_3_2_3
+{
+    static void Main(string[] args)
+    {
+
+        int[] mas = new int[12] { 1, 1, 1, 4, 5, 6, 7, 8, 9, 10, 11, 1 };
+        int count = 0;
+
+        for (int k = 0; k < mas.Length; k++)
+        {
+            for (int i = 0; i < mas.Length; i++)
+
+                if (mas[k] == mas[i] && k != i)
+                {
+                    count++;
+                    break;
+                }
+        }
+
+        if (count != 0)
+            Console.WriteLine("Количество элементов массива, встречающихся в нем один раз - " + (mas.Length - count));
+        else if (count == 0)
+            Console.WriteLine("Количество элементов массива, встречающихся в нем один раз - " + (mas.Length));
+    }
+}
